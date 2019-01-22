c6320708835e2b63319057f5a2886ff6f25e2c34
Tamari Tinsley <tamaritinsley16@gmail.com>
2019-01-21 23:16:20 -0800

    updated prompt

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let randomNumber = Math.floor(Math.random() * 10 + 1);
-var userInput = prompt('Guss the Number');
+var userInput = prompt('Guss the Number between 1 and 10');
 if (userInput == randomNumber) {
     document.write('<h1> ' + "You Guessed the correct number! : " + randomNumber  + '</h1>');
 } else {
