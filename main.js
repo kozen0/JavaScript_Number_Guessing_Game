232ae7bb258b12a3d6228c9f73a8105c35967717
Tamari Tinsley <tamaritinsley16@gmail.com>
2019-01-21 23:40:22 -0800

    updated app

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let randomNumber = Math.floor(Math.random() * 10 + 1);
-var userInput = prompt('Guss the Number between 1 and 10');
+do {
+  var randomNumber = Math.floor(Math.random() * 10 + 1);
+  var userInput = prompt('Guss the Number between 1 and 10');
+  if (userInput != randomNumber) {
+      alert('thats not right');
+  }
+} while (userInput != randomNumber);
 if (userInput == randomNumber) {
     document.write('<h1> ' + "You Guessed the correct number! : " + randomNumber  + '</h1>');
-} else {
-    document.write('<h1>' + "Thats not right" + '</h1>')
 }
